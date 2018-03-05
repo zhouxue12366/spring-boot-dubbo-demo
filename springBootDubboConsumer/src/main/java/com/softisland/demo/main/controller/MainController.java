4ead2e9cacf0ef4607c6cc91d732eb94a3ae6052
zhougao <2885253480@qq.coom>
2018-03-05 09:33:49 +0800

    上周写的代码，现在忘记了；好像优化了一部分代码；

--- springBootDubboConsumer/src/main/java/com/softisland/demo/main/controller/MainController.java
+++ springBootDubboConsumer/src/main/java/com/softisland/demo/main/controller/MainController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.jfinal.plugin.activerecord.Record;
@@ -24,9 +25,22 @@ public class MainController {
 			return null;
 		}
 		List<Record> records = demoService.sayHello(name);
-		
 		System.out.println(name+">查询数据为:"+records);
 		return records;
+	}
+	
+	@RequestMapping("/index")
+	public ModelAndView index(String name) {
+		ModelAndView view = new ModelAndView("views/index.html");
+		if (null == demoService) {
+			System.out.println("引用对象为:" + demoService);
+			return null;
+		}
+		List<Record> records = demoService.sayHello(name);
+		System.out.println(name+">查询数据为:"+records);
+		view.addObject("records", records);
+		view.addObject("name", name);
+		return view;
 	}
 
 }
